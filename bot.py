f08e02b02f2b552c0e584945ffcc0ea087736ee4
Janobim <abufuzaylkhan@gmail.com>
2025-12-11 00:09:27 +0500

    Fix polling for Railway

--- bot.py
+++ bot.py
@@ -1,24 +1,18 @@
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(
-        KeyboardButton("Profil"),
-        KeyboardButton("Kaloriyani hisoblash"),
-    )
-    markup.add(
-        KeyboardButton("Taom qo‘shish"),
-        KeyboardButton("Statistika")
-    )
-    bot.reply_to(message, "Assalomu alaykum Janobim! Menyuni tayyorlab qo‘ydim.", reply_markup=markup)
+    bot.reply_to(message, "Salom! Bot ishlayapti.")
 
-bot.infinity_polling()
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+print("Bot started...")
+
+bot.polling(none_stop=True, interval=0)
